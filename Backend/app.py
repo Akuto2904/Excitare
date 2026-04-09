4986202e6d0abc9ef5dec5e53af6d45f92533134
Jedidiah-1234 <Jedidiahdaly1@gmail.com>
2026-04-09 16:55:36 +0100

    Added Working Google API Endpoints and Connected google account refresh token to user 1

--- Backend/app.py
+++ Backend/app.py
@@ -1,14 +1,54 @@
 import os
 import json
-from flask import Flask, jsonify, render_template, request, url_for
+from flask import Flask, jsonify, render_template, request, url_for, session, redirect
 from sqlalchemy import select, func
 from flask_restful import Api, Resource
 from models import db, Alarm, User, Review
 from auth import require_api_key  # Import middleware from auth.py
 from flask_cors import CORS
 
+# Google Calendar Implementation
+from datetime import datetime, timedelta 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import Flow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from dotenv import load_dotenv
+
+load_dotenv()
+
+COOKIE_KEY = os.getenv('COOKIE_KEY')
+GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+GOOGLE_PROJECT_ID = os.getenv('GOOGLE_PROJECT_ID')
+GOOGLE_AUTH_URI = os.getenv('GOOGLE_AUTH_URI')
+GOOGLE_TOKEN_URI = os.getenv('GOOGLE_TOKEN_URI')
+GOOGLE_AUTH_PROVIDER_X509_CERT_URL = os.getenv('GOOGLE_AUTH_PROVIDER_X509_CERT_URL')
+GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
+GOOGLE_REDIRECT_URIS = os.getenv('GOOGLE_REDIRECT_URIS')
+GOOGLE_JAVASCRIPT_ORIGINS = os.getenv('GOOGLE_JAVASCRIPT_ORIGINS')
+
+clientConfig = {
+    "web": {
+        "client_id":GOOGLE_CLIENT_ID,
+        "project_id":GOOGLE_PROJECT_ID,
+        "auth_uri":GOOGLE_AUTH_URI,
+        "token_uri":GOOGLE_TOKEN_URI,
+        "auth_provider_x509_cert_url":GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+        "client_secret":GOOGLE_CLIENT_SECRET,
+        "redirect_uris":[GOOGLE_REDIRECT_URIS],
+        "javascript_origins":[GOOGLE_JAVASCRIPT_ORIGINS]
+    }
+}
+
+# Allow Google login from HTTP
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
 # Create a Flask application instance
 app = Flask(__name__)
+# Used for saving session
+app.secret_key = COOKIE_KEY
+
 CORS(app)
 
 # Store the database inside the project directory (database/database.db)
@@ -290,6 +330,8 @@ def index():
     
     return render_template('index.html', alarms=alarms, users=users, reviews=reviews)
 
+
+
 # Retrives alarm json given alarm ID in url
 @app.route('/api/alarm/<int:id>', methods = ['GET'])
 @require_api_key  # Applies middleware
@@ -569,6 +611,138 @@ def deleteAlarmReview():
                 }
             ]
         })
+
+# Send user here and itll redirect to google authentication
+@app.route('/api/<string:userID>/calendarLogin')
+#@require_api_key
+def calendarLogin(userID):
+    flow = Flow.from_client_config(clientConfig, scopes="https://www.googleapis.com/auth/calendar.readonly")
+    flow.redirect_uri = url_for('calendarLoginRedirect', _external=True)
+
+    googleAuth, state = flow.authorization_url(prompt='consent', state=userID, access_type="offline", include_granted_scopes="true")
+    session['code_verifier'] = flow.code_verifier
+    
+    return redirect(googleAuth)
+
+
+# User is sent here after google authentication, saves credentials to database then redirects
+@app.route('/calendarLoginRedirect')
+def calendarLoginRedirect():
+    userID = request.args.get('state')
+
+    flow = Flow.from_client_config(clientConfig, scopes="https://www.googleapis.com/auth/calendar.readonly")
+    flow.redirect_uri = url_for('calendarLoginRedirect', _external=True)
+    
+    loggedInStatus = request.url
+    flow.fetch_token(authorization_response=loggedInStatus, code_verifier=session['code_verifier'])
+   
+    # Retrieves current user from database
+    user = User.query.filter_by(id=userID).first()
+    # Assigns current users google refresh token to user in db
+    user.refreshToken = flow.credentials.refresh_token
+    # Saves refresh token to db
+    db.session.commit()
+
+
+    # Change this return to a redirect to the front end
+    return jsonify({"Message": "Log In Success"})
+
+@app.route('/api/<string:userID>/calendars', methods = ['GET'])
+@require_api_key
+def calendars(userID):
+
+    # Retrieves current user from url and retrieves user from database
+    user = User.query.filter_by(id=userID).first()
+
+    # Assembles credentials object using db refresh token 
+    sessionCredentials = Credentials(token=None, refresh_token=user.refreshToken, client_id=GOOGLE_CLIENT_ID, client_secret=GOOGLE_CLIENT_SECRET, token_uri=GOOGLE_TOKEN_URI)
+    
+    try:
+        service = build("calendar", "v3", credentials=sessionCredentials)
+        allCalendars = service.calendarList().list(fields="items(id,summary)").execute()
+        niceOutput = []
+        for calendar in allCalendars.get("items", []):
+            calendarInfoAsDict={
+                    "id" : calendar.get("id"),
+                    "summary" : calendar.get("summary")
+                }
+            niceOutput.append(calendarInfoAsDict)
+
+        return jsonify(niceOutput)
+        
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return jsonify({(f"An error occurred: {error}")})
+
+# Given a calendarID in a JSON, sets the user in url's chosen calendar
+@app.route('/setCalendar/<string:userID>', methods = ['PUT'])
+@require_api_key
+def setCalendar(userID):
+    requestMade = request.json
+    givenCalendarID = requestMade.get("id")
+
+    matchingUser = User.query.get(userID)
+    if not matchingUser:
+        return {"error": "User not found"}
+
+    matchingUser.chosenCalendarID = givenCalendarID
+
+    # Commit the changes
+    db.session.commit()
+
+    return jsonify(
+        {"message": "User updated successfully!",
+            "_links": [
+                {
+                "href": f"/api/users/{matchingUser.id}",
+                "rel": "this",
+                "method": "GET",
+                },
+                {
+                "href": f"/api/{matchingUser.id}/firstClass",
+                "rel": "this",
+                "method": "GET",
+                }
+            ]
+        }
+    )
+
+# Retrieves the name and the time of the first event scheduled the next day for the user in url, returns as JSON
+@app.route('/api/<string:userID>/firstClass', methods = ['GET'])
+@require_api_key
+def classTime(userID):
+    
+    # Retrieves current user from url and retrieves user from database
+    user = User.query.filter_by(id=userID).first()
+    # Assembles credentials object using db refresh token 
+    sessionCredentials = Credentials(token=None, refresh_token=user.refreshToken, client_id=GOOGLE_CLIENT_ID, client_secret=GOOGLE_CLIENT_SECRET, token_uri=GOOGLE_TOKEN_URI)
+    
+    givenCalendarID=user.chosenCalendarID
+
+    try:
+        service = build("calendar", "v3", credentials=sessionCredentials)
+        tomorrow = (datetime.now() + timedelta(days=1)).replace(hour=0, minute=0).isoformat() + 'Z'
+
+        classesAsList = service.events().list(calendarId=givenCalendarID, timeMin=tomorrow, maxResults=1, singleEvents=True, orderBy="startTime").execute()
+        classes = classesAsList.get("items", [])
+
+        if not classes:
+            print("No class tomorrow")
+            return jsonify({"message" : "No class tomorrow"},{"status" : 0})
+
+        start = classes[0]["start"].get("dateTime", classes[0]["start"].get("date"))
+        classInfo = {
+            "startTime" : start,
+            "class" : classes[0]["summary"],
+            "status" : 1
+        }
+        
+        return jsonify(classInfo)
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return jsonify({(f"An error occurred: {error}")})
+
 
 # When this script(app.py) is run
 # Database tables are created before running
