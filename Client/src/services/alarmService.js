f65c896c0c63306229ba411f04951c22bad116d3
Lois-c982 <2638924@dundee.ac.uk>
2026-04-09 11:46:06 +0100

    Current alarm button

--- Client/src/services/alarmService.js
+++ Client/src/services/alarmService.js
@@ -7,3 +7,11 @@ export const getAlarms = async () => {
 export const getAlarmById = async (id) => {
   return await apiRequest(`/alarm/${id}`);
 };
+
+// Updates a user's chosen alarm
+export const updateUserChosenAlarm = async (userData) => {
+  return await apiRequest('/users', {
+    method: 'PUT',
+    body: JSON.stringify(userData),
+  });
+};
